09cf946c5f6d827b7a15ab2be11cb38f9b5bf38f
aaaminov <neverket@gmail.com>
2021-09-17 21:52:00 +0500

    1.2

--- homework17.09/src/com/company/Main.java
+++ homework17.09/src/com/company/Main.java
@@ -3,9 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Plant tree = new Tree("для бумаги");
-        Plant brush = new Bush("яблоко");
-        Plant flower = new Flower(10, true);
+        Plant tree = new Tree("дуб", "России","для бумаги");
+        Plant brush = new Bush("яблоня", "России", "яблоко");
+        Plant flower = new Flower("Мак","России", 10, true);
         System.out.print( tree.getData() + brush.getData() + flower.getData());
     }
 }
--- homework17.09/src/com/company/Plant.java
+++ homework17.09/src/com/company/Plant.java
@@ -1,29 +1,40 @@
 package com.company;
 
 public abstract class Plant {
+    String name;
+    String habitat;
+
+    public Plant(String name, String habitat) {
+        this.name = name;
+        this.habitat = habitat;
+    }
+
     public abstract String getData();
 }
 class Tree extends Plant {
     String howUsed;
 
-    public Tree(String howUsed) {
+    public Tree(String name, String habitat, String howUsed) {
+        super(name, habitat);
         this.howUsed = howUsed;
     }
     @Override
     public String getData() {
-        return "Как используется: " + howUsed + "\n";
+        return name + " , растет в " + habitat +  ", как используется: " + howUsed + "\n";
     }
 }
 
 class Bush extends Plant {
     String typeOfFruit;
 
-    public Bush(String typeOfFruit) {
+    public Bush(String name, String habitat, String typeOfFruit) {
+        super(name, habitat);
         this.typeOfFruit = typeOfFruit;
     }
+
     @Override
     public String getData() {
-        return "Тип плода: " + typeOfFruit + "\n";
+        return name + " , растет в " + habitat +  ", тип плода: " + typeOfFruit + "\n";
     }
 }
 
@@ -31,13 +42,16 @@ class Flower extends Plant {
     int lifeExpectancy;
     boolean hasSmell;
 
-    public Flower(int lifeExpectancy, boolean hasSmell) {
+    public Flower(String name, String habitat, int lifeExpectancy, boolean hasSmell) {
+        super(name, habitat);
         this.lifeExpectancy = lifeExpectancy;
         this.hasSmell = hasSmell;
     }
+
     @Override
     public String getData() {
-        return "Продолжительность жизни = " + Integer.toString(lifeExpectancy) +
+        return name + " , растет в " + habitat +
+                ", продолжительность жизни = " + Integer.toString(lifeExpectancy) +
                 ", имеет ли запах: " + Boolean.toString(hasSmell);
     }
 }
