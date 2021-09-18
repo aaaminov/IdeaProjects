c648a82a4640a15031b9427b9cc98ed3bbf31af9
aaaminov <neverket@gmail.com>
2021-09-18 13:08:30 +0500

    18.09

--- Homework_06.09/src/com/company/Main.java
+++ Homework_06.09/src/com/company/Main.java
@@ -5,52 +5,52 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner ex_line = new Scanner(System.in);
-        int ex = ex_line.nextInt(); //задание
-        if (ex == 1){
-            Scanner str_line = new Scanner(System.in);
-            String str = str_line.nextLine();
-            int count_rus_char = 0;
-            for (int i = 0; i < str.length(); i++){
-                int str_i = (int)str.charAt(i);
-                if (1072 <= str_i && str_i <= 1103){ //а-я = [1072-1103]
-                    count_rus_char++;
-                }
+        System.out.println("Введите строку: ");
+        Scanner str_line = new Scanner(System.in);
+        String str = str_line.nextLine();
+        int count_rus_char = 0;
+        for (int i = 0; i < str.length(); i++){
+            int str_i = (int)str.charAt(i);
+            if (1072 <= str_i && str_i <= 1103){ //а-я = [1072-1103]
+                count_rus_char++;
             }
-            System.out.println(count_rus_char);
-            //str_line.close();
         }
-        else if (ex == 2){
-            String str = "";
-            Scanner num_line = new Scanner(System.in);
-            int num = num_line.nextInt();
-            Scanner char1_line = new Scanner(System.in);
-            char char1 = char1_line.nextLine().charAt(0);
-            Scanner char2_line = new Scanner(System.in);
-            char char2 = char2_line.nextLine().charAt(0);
-            for (int i = 0; i < num; i++){
-                str = str + char1 + char2;
+        System.out.println("кол-во маленьких русских символов: " + count_rus_char);
+        //str_line.close();
+
+        String str2 = "";
+        System.out.println("Введите число: ");
+        Scanner num_line = new Scanner(System.in);
+        int num = num_line.nextInt();
+        System.out.println("Введите первый символ: ");
+        Scanner char1_line = new Scanner(System.in);
+        char char1 = char1_line.nextLine().charAt(0);
+        System.out.println("Введите второй символ: ");
+        Scanner char2_line = new Scanner(System.in);
+        char char2 = char2_line.nextLine().charAt(0);
+        for (int i = 0; i < num; i++){
+            str2 = str2 + char1 + char2;
+        }
+        System.out.println("полученная строка: " + str2);
+        //num_line.close();
+        //char1_line.close();
+        //char2_line.close();
+
+        System.out.println("Введите строку: ");
+        Scanner str_line3 = new Scanner(System.in);
+        String str3 = str_line3.nextLine();
+        System.out.println("Введите искомый символ: ");
+        Scanner char_line = new Scanner(System.in);
+        char search_char = char_line.nextLine().charAt(0);
+        int count_search_char = 0;
+        for (int i = 0; i < str3.length(); i++){
+            if (str3.charAt(i) == search_char){
+                count_search_char++;
             }
-            System.out.println(str);
-            //num_line.close();
-            //char1_line.close();
-            //char2_line.close();
         }
-        else if (ex == 3){
-            Scanner str_line = new Scanner(System.in);
-            String str = str_line.nextLine();
-            Scanner char_line = new Scanner(System.in);
-            char search_char = char_line.nextLine().charAt(0);
-            int count_search_char = 0;
-            for (int i = 0; i < str.length(); i++){
-                if (str.charAt(i) == search_char){
-                    count_search_char++;
-                }
-            }
-            System.out.println(count_search_char);
-            //str_line.close();
-            //char_line.close();
-        }
-        ex_line.close();
+        System.out.println("кол-во вхождений символа: " + count_search_char);
+        //str_line.close();
+        //char_line.close();
+
     }
 }
